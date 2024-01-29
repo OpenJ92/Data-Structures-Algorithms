58203b1dbc21f0e1e961cbd60d4c6efd07d2a76e
OpenJ92 <jacob.vartuli.92@gmail.com>
2024-01-29 08:17:32 -0500

    Update 0007

--- problems/python/0007.py
+++ problems/python/0007.py
@@ -14,3 +14,23 @@ class Solution:
             return 0
         else:
             return int(reversed_x)
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        parity = 1 if x > 0 else -1
+        x = parity * x
+
+        digits = []
+        while x > 0:
+            digits = [(x % 10), *digits]
+            x = (x - (x % 10)) // 10
+
+        reversed_x = 0
+        for index, digit in enumerate(digits):
+            reversed_x += digit*(10**index)
+        reversed_x *= parity
+
+        if reversed_x > 2**31 - 1 or reversed_x < - 2**31:
+            return 0
+        return reversed_x
+
