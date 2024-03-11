bb201823eb37de2b9dab0a8da1fdb4bc64e920ec
Jacob Vartuli <jacobvartuli@macintosh.local>
2024-03-10 21:15:56 -0400

    daily

--- problems/python/0791.py
+++ problems/python/0791.py
@@ -1,16 +1,30 @@
+## class Solution:
+##     def customSortString(self, order: str, string: str) -> str:
+##         collect = defaultdict(list)
+##         others = []
+## 
+##         for letter in string:
+##             if letter in order:
+##                 collect[letter].append(letter)
+##             else:
+##                 others.append(letter)
+## 
+##         ordered_letters = []
+##         for letter in order:
+##             ordered_letters.append("".join(collect[letter]))
+## 
+##         return "".join([*ordered_letters, *others])
+
 class Solution:
     def customSortString(self, order: str, string: str) -> str:
-        collect = defaultdict(list)
-        others = []
-
-        for letter in string:
-            if letter in order:
-                collect[letter].append(letter)
-            else:
-                others.append(letter)
+        collect = Counter(string)
 
         ordered_letters = []
         for letter in order:
-            ordered_letters.append("".join(collect[letter]))
+            ordered_letters.append(collect[letter] * letter)
+            del collect[letter]
+        for letter in collect:
+            ordered_letters.append(collect[letter] * letter)
 
-        return "".join([*ordered_letters, *others])
+        return "".join(ordered_letters)
+
