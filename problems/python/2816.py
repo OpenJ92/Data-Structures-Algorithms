4d870472328cfc61a338e8bd5f7c7329582d74f8
OpenJ92 <jacob.vartuli.92@gmail.com>
2024-03-20 18:01:13 -0400

    linked list problem

---
+++ problems/python/2816.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        def backtrack(node):
+            if not node:
+                return 0, None
+
+            carry, npde = backtrack(node.next)
+            carry, number = divmod(carry + 2 * node.val, 10)
+            node.val, node.next = number, npde
+
+            return carry, node
+
+        carry, node = backtrack(head)
+        if not carry:
+            return node
+        return ListNode(carry, node)
