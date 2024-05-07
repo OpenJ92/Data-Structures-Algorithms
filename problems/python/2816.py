1d835d511f21e6464969bc3609e0193ef47826d9
OpenJ92 <jacob.vartuli.92@gmail.com>
2024-05-06 21:40:07 -0400

    daily

--- problems/python/2816.py
+++ problems/python/2816.py
@@ -3,24 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        def backtrack(node):
-            if not node:
-                return 0, None
-
-            carry, npde = backtrack(node.next)
-            carry, number = divmod(carry + 2 * node.val, 10)
-            node.val, node.next = number, npde
-
-            return carry, node
-
-        carry, node = backtrack(head)
-        if not carry:
-            return node
-        return ListNode(carry, node)
-
 class Inplace:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
@@ -35,6 +17,24 @@ class Inplace:
             return carry
 
         carry = backtrack(head)
+        if not carry:
+            return head
+        return ListNode(carry, head)
+
+
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack, node = [], head
+        while node:
+            stack.append(node)
+            node = node.next
+
+        carry = 0
+        while stack:
+            node = stack.pop()
+            carry, number = divmod(carry + 2 * node.val, 10)
+            node.val = number
+
         if not carry:
             return head
         return ListNode(carry, head)
